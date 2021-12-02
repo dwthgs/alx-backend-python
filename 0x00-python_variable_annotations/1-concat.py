3c37e718eda791e440a5568e7f69c6e33cfbc107
dev <dev@devs.local>
2021-12-02 14:36:03 +0300

    type-annotated function concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
-"""  """
+""" Basic annotations - concat """
 
 
 def concat(str1: str, str2: str) -> str:
-    """  """
+    """
+    Parameters
+    ----------
+    str1: str
+    str2: str
+
+    Return: a concatenated string
+    """
     return str1 + str2
