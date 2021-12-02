fc94717310378aca8ca75133faad42021705f384
dev <dev@devs.local>
2021-12-02 15:02:53 +0300

    add type annotations to the function

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""  """
+""" More involved type annotations """
 from typing import Any, Mapping, Union, TypeVar
 
 T = TypeVar('T')
@@ -8,8 +8,16 @@ T = TypeVar('T')
 def safely_get_value(
         dct: Mapping,
         key: Any,
-        default: Union[T, None]=None) -> Union[Any, T]:
-    """  """
+        default: Union[T, None] = None) -> Union[Any, T]:
+    """
+    Parameters
+    ----------
+    dct: Mapping
+    key: Any
+    default: T or None
+
+    Return: the key in dct or default
+    """
     if key in dct:
         return dct[key]
     else:
