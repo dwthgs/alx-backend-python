9898ef1c4ebb11bf88d96977f2fb5fb7d04b0971
dev <dev@devs.local>
2021-12-06 21:31:28 +0300

    measure runtime

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" 2. Measure the runtime """
+
+import asyncio
+from time import perf_counter
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """ Measure runtime """
+    c = perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    return perf_counter() - c
