048a785636e14d4c1a812040b4532399d9243978
dev <dev@devs.local>
2021-12-06 21:17:35 +0300

    wait_random

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" 0. The basics of async """
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """ waits for a random delay between 0 and max_delay """
+    n = random.uniform(0, max_delay)
+    await asyncio.sleep(n)
+    return n
